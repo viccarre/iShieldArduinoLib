568e0cc895baadb570bb3b02d4f20eb0db4b0ee3
viccarre <viccarre@gmail.com>
2015-06-20 23:04:58 -0400

    examples added

--- iShieldResources/iShield/examples/LED1/LED1.ino
+++ iShieldResources/iShield/examples/LED1/LED1.ino
@@ -7,34 +7,64 @@
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
-
-//"services.h/spi.h/boards.h" is needed in every new project
 #include <SPI.h>
-#include <boards.h>
+#include <Nordic_nRF8001.h>
 #include <RBL_nRF8001.h>
-#include <iShield.h>
-
+#include "iShield.h"
 
 iShield myiShield;
-int potPin = 0;
-int val;
+
+int LED1 = 2;
+int LED2 = 3;
+int LED3 = 4;
+int LED4 = 5;
 
 void setup()
 {
-    // Init. and start BLE library.
-  ble_begin();
-    Serial.begin(9600); 
-    ble_set_name("BLEShield");
+  	// Init. and start BLE library.
+	ble_begin();
+  	Serial.begin(9600); 
+  	ble_set_name("BLEShield");
+  	
+  	pinMode(LED1, OUTPUT);
+  	pinMode(LED2, OUTPUT); 
+  	pinMode(LED3, OUTPUT); 
+  	pinMode(LED4, OUTPUT);
+  	
+  	digitalWrite(LED1,LOW);
+  	digitalWrite(LED2,LOW);
+  	
 }
 
 void loop()
 {
-  //val = (analogRead(potPin)/4);
-  //Serial.println(val);
-  for(int i = 0;i<256;i++){
-    myiShield.setGaugeValue(i);
-    delay(100);
-  }
-  //myiShield.setGaugeValue(val);
-
+	myiShield.getInbox();
+	
+	//Buttons
+	if(myiShield.isButton1Pressed()){
+	    digitalWrite(LED1,HIGH);
+    }else{
+    	digitalWrite(LED1,LOW);
+    }
+      
+    if(myiShield.isButton2Pressed()){
+	    digitalWrite(LED2,HIGH);
+    }else{
+    	digitalWrite(LED2,LOW);
+    }
+    
+    //Switches
+	if(myiShield.isSwitch1On()){
+    	digitalWrite(LED3,HIGH);
+    }else{
+    	digitalWrite(LED3,LOW);
+    }
+    
+    if(myiShield.isSwitch2On()){
+    	digitalWrite(LED4,HIGH);
+    }else{
+    	digitalWrite(LED4,LOW);
+    }
+    
+    
 }
--- iShieldResources/iShield/examples/RGB_LED/RGB_LED.ino
+++ iShieldResources/iShield/examples/RGB_LED/RGB_LED.ino
@@ -7,34 +7,56 @@
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
-
-//"services.h/spi.h/boards.h" is needed in every new project
 #include <SPI.h>
-#include <boards.h>
+#include <Nordic_nRF8001.h>
 #include <RBL_nRF8001.h>
-#include <iShield.h>
-
+#include "iShield.h"
 
 iShield myiShield;
-int potPin = 0;
-int val;
+
+int redPin = 2;
+int greenPin = 3;
+int bluePin = 4;
+
+int red, green, blue;
 
 void setup()
 {
-    // Init. and start BLE library.
-  ble_begin();
-    Serial.begin(9600); 
-    ble_set_name("BLEShield");
+  	// Init. and start BLE library.
+	ble_begin();
+  	Serial.begin(9600); 
+  	ble_set_name("BLEShield");
+  	
+	pinMode(redPin, OUTPUT);
+  	pinMode(greenPin, OUTPUT);
+  	pinMode(bluePin, OUTPUT); 
+  	
+  	red = 0;
+  	green = 0;
+  	blue = 0;
+  	
 }
 
 void loop()
 {
-  //val = (analogRead(potPin)/4);
-  //Serial.println(val);
-  for(int i = 0;i<256;i++){
-    myiShield.setGaugeValue(i);
-    delay(100);
-  }
-  //myiShield.setGaugeValue(val);
+	myiShield.getInbox();
+	
+	red = myiShield.getRedValue();
+	green = myiShield.getGreenValue();
+	blue = myiShield.getBlueValue();
+	
+	setColor(red, green, blue);
 
+}
+
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
 }
